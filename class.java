1a68546a62f98dde56dbb110c94d4af90e3333a5
Samruddhi Chougale <samruddhich1308@gmail.com>
2025-06-26 23:08:40 +0530

    Add files via upload

---
+++ class.java
@@ -0,0 +1,16 @@
+// simple class
+class student{
+    int id=10;
+    String name="radha";
+    public static void main(String[]args)
+    {
+       student std=new student();   //creating object of student
+       System.out.println(std.id +"  " +std.name);
+    }
+
+    }
+
+
+
+    
+
