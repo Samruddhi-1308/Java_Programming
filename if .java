711e25c1b25ab1fa58762c62d3daf95b4eadfe80
Samruddhi Chougale <samruddhich1308@gmail.com>
2025-06-26 23:18:52 +0530

    Add files via upload

---
+++ if .java
@@ -0,0 +1,20 @@
+
+class test
+{
+    public static void main(String[] args) {
+        
+        int num=20;
+        if(num==0)
+        {
+            System.out.println("number is o");
+        }
+        if(num>0)
+        {
+            System.out.println("number is positive");
+        }
+        if(num<0)
+        {
+            System.out.println("number is negative");
+        }
+    }
+}
