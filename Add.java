e958fcad557681ce18c3b91464802e4831c419a2
Samruddhi Chougale <samruddhich1308@gmail.com>
2025-06-26 23:07:31 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Add
+{
+    public static void main(String[] args) 
+    {
+
+
+    int a,b,c;
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter first no:");
+    a=sc.nextInt();
+    System.out.println("second no:");
+    b=sc.nextInt();
+    c=a+b;
+    System.out.println("Addition="+c);
+    
+    sc.close();
+    
+
+    }
+     
+}
+    
+
