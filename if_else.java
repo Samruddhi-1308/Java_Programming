1f24e77dd66c406504d7b881d41ce8cd290b4041
Samruddhi Chougale <samruddhich1308@gmail.com>
2025-06-26 23:21:01 +0530

    Add files via upload

---
+++ if_else.java
@@ -0,0 +1,44 @@
+// // no is large and small
+// public class if_else
+// {
+
+//     public static void main(String []args)
+//     {
+        
+//         int n1 = 10;
+//         int n2 = 15;
+//         if(n1>n2)
+//             {
+//                 System.out.println("num1 is greter than num2");
+//             }
+//             else
+//             {
+//                 System.out.println("num1 less than num2");
+//             }
+//     }
+
+// }
+
+
+// even odd number
+class if_else
+{
+    public static void main(String[] args)
+    {
+        int num1=23;
+        int num2=20;
+        if(num1%2==0)
+        {
+            if(num2%2==0)
+            {
+                System.out.println("number is even");
+            }
+            
+        }
+        else
+        {
+            System.out.println("number is odd");
+        }
+        
+    }
+}
