04353adef6f5aa808e1bbce03e0e5431b28f9a98
Samruddhi Chougale <samruddhich1308@gmail.com>
2025-06-26 23:24:22 +0530

    Add files via upload

---
+++ swap_no.java
@@ -0,0 +1,24 @@
+public class swap_no
+{
+    public static void main(String[]args)
+    {
+        int a=10,b=15,temp;
+        System.out.println("number before swapping");
+        System.out.println(a);
+        System.out.println(b);
+
+        
+        temp=a;
+        a=b;
+        b=temp;
+    
+
+        System.out.println("number after swapping");
+        System.out.println(a);
+        System.out.println(b);
+
+
+
+    }
+    
+}
